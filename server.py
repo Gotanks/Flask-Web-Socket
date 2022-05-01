92d10ac8307ca94125b084ae3e35fdba78347e97
Gotanks <34191122+Gotanks@users.noreply.github.com>
2022-05-01 11:58:05 -0400

    Update server.py
    Fixed import typos

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
 #make a simple server application using Python. Use the socket library or flask. All the server has is a dictionary which contains a toy and the quantity of that toy. The client should be able to use a GET request to the server to see how many of a toy it has.
 from flask import Flask
-from flask_sock import Socl
+from flask import Sock
 
 app = Flask(__name__)
 sock = Sock(app)
 
-#sample server client name
+#sample server client name 
 @sock.route('/Characters')
 def data(d):
     while True:
-        Charlist = ["Mario", "Luigi", "Donkey Kong", "Yoshi" "Link", "Kirby", "Fox", "Pikachu", "Jigglypuff", "Samus", "Captain Falcon", "Ness"]
-        for i in range(len(Charlist)):
-            Charlist[i] = Charlist[i].lower()
+        charlist = ["Mario", "Luigi", "Donkey Kong", "Yoshi" "Link", "Kirby", "Fox", "Pikachu", "Jigglypuff", "Samus", "Captain Falcon", "Ness"]
+        for i in range(len(charlist)):
+            charlist[i] = charlist[i].lower()
         input = d.receive()
         input = input.lower()
         # loops through the character list for the name of character
-        match = [name for name in Charlist if(name in input)]
+        match = [name for name in charlist if(name in input)]
         match = [match.title() for match in match]
-   
