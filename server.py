c2a98ec23bba616d843c5c310ed435ec2494e429
Gotanks <golbaridavid@gmail.com>
2022-05-01 19:52:53 -0400

    Changes made to POST method

--- server.py
+++ server.py
@@ -3,18 +3,20 @@ from flask import *
 app = Flask(__name__)
 #sample server client name 
 @app.route('/', methods = ['POST', 'GET'])
-def data(d):
+def data():
     while True:
-        charlist = {'Mario': 1, 'Luigi': 2, 'Bowser': 3}
+        charlist = {'Ganondorf': 1, 'Falco': 2, 'Bowser': 3}
         for i in range(len(charlist)):
             charlist[i] = charlist[i].lower()
+        charname = charlist.get('name')
+        charnumber = charlist.get('number')
         if request.method == 'GET':
             return print("These are the characters"), charlist
         if request.method == 'POST':
-            input = d.receive()
-            input = input.lower()
-        if input in charlist:
-            input.title()
-        print(input + "is in the list")
+            # input = d.receive()
+            # input = input.lower()
+            if charlist in charlist(charname, charnumber):
+                result = {key: value for key, value in charlist.items}
+        print(result + "is in the list")
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
