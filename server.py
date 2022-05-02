e1edcf6ffeef1d0d2d8ba02e2e60a676f1888810
Gotanks <golbaridavid@gmail.com>
2022-05-01 21:34:54 -0400

    Fixed lowercase problem, and implemented dictionary

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 #Make a simple server application using Python. Use the socket library or flask. All the server has is a dictionary which contains a toy and the quantity of that toy. The client should be able to use a GET request to the server to see how many characters it has.
 from flask import *
 app = Flask(__name__)
-charlist = {'Ganondorf': 1, 'Falco': 2, 'Bowser': 3}
-# for i in range(len(charlist)):
-#     charlist[i] = charlist[i].lower()
+# charlist = {'Ganondorf': 1, 'Falco': 2, 'Bowser': 3}
+charlist = {
+    "Bowser": 1,
+    "Ganondorf": 2,
+    "Falco": 3
+}
+
+charlist = dict((k.lower(), v) for k, v in charlist.items())
 #sample server client name 
 @app.route('/data', methods = ['POST', 'GET'])
 def data():
@@ -11,14 +16,11 @@ def data():
         args = request.args
         charname = args.get('name')
         charnumber = args.get('number')
-        if request.method == 'GET':
-            return 'You reached the GET spot'
-        elif request.method == 'POST':
-            # input = d.receive()
-            # input = input.lower()
-            if charlist in charlist(charname, charnumber):
+        if request.method == 'POST':
+            if charname in charlist(charname, charnumber):
                 result = {key: value for key, value in charlist.items}
-                return 'You reached the POST spot'
+        elif request.method == 'GET':
+            return charlist
         return result.values
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
