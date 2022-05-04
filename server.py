9dd7d688704bf489aec36689b6d75dbb693ab6d8
Gotanks <golbaridavid@gmail.com>
2022-05-03 23:01:13 -0400

    Finished first task, on to the next phase for JSON

--- server.py
+++ server.py
@@ -1,44 +1,25 @@
 #Make a simple server application using Python. Use the socket library or flask. All the server has is a dictionary which contains a toy and the quantity of that toy. The client should be able to use a GET request to the server to see how many characters it has.
 #Make get request for client to query through ex: if user types in value in the url, the server returns key
+# Read up on JSON for more information on how the JSON ONLY TWO LINES NEED TO BE MODIFIED
 from flask import *
 app = Flask(__name__)
 charlist = {
-    "Bowser": "Melee",
-    "Ganondorf": "Melee",
-    "Falco": "Melee",
-    "Lucario": "Brawl",
-    "Ike": "Brawl",
-    "Cloud": "Sm4sh",
-    "Ridley": "Ultimate",
-    "Kazuya": "Ultimate"
+    "Bowser": 64,
+    "Ganondorf": 3,
+    "Falco": 10,
+    "Lucario": 3,
+    "Ike": 0,
+    "Cloud": 1,
+    "Ridley": 5,
+    "Kazuya": 2
 }
-
-# charlist = dict((k.lower(), v) for k, v in charlist.items())
-#sample server client name 
-@app.route('/', methods = ['GET', 'POST'])
-def data():
-    args = request.args
-    charname = request.args.get('character')
-    chargame = request.args.get('series')
-    result = charlist
-    if request.method == 'POST':
-        return result
-    if None not in (charname, chargame):
-        result = {k: v for k, v in charlist.items() if k == charname and v == chargame}
-            # return result
-    elif charname is not None:
-        result = {k: v for k, v in charlist.items() if k == charname}
-         # return result.keys
-    elif chargame is not None:
-        result = {k: v for k, v in charlist.items() if v == chargame}
-    return result
-
-#Current bug here is if you mix the parameters around then the returned key and value are mixed
-@app.route('/select/<character>/<series>', methods = ['GET'])
-def character_select(series, character):
-    return "First introduced in: " + series
-def character_series(character, series):
-    return "Character: " + character 
+#TODO return a json of characters when returning name and quantity as the fields, if the key does not exist, then return saying item doesn't exist
+@app.route('/character/<char>', methods = ['GET'])
+def checkseries(char):
+    for key, value in charlist.items():
+        if key.lower() == char.lower():
+            return "Quantity left is: " + str(value)
+    return "CHARACTER NOT FOUND"
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
